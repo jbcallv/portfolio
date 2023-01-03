01c276ea81a66993e754de746cb0d7f86e81727e
jbcallv <jbcallv@gmail.com>
2023-01-03 05:44:22 -0500

    projects button

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -19,14 +19,31 @@
         <div class="text-h2 text-weight-light q-pb-lg gt-sm">
           Hi, I'm Joseph.
         </div>
-        <div class="text-h3 text-weight-light q-pb-lg lt-md">
+        <div class="text-h3 text-center text-weight-light q-pb-lg lt-md">
           Hi, I'm Joseph.
         </div>
 
-        <div class="text-h5 text-weight-light text-grey">
+        <div class="text-h5 text-weight-light text-grey gt-sm q-pb-lg">
           Full Stack Developer from Virginia, USA. I create web applications
           with modern frameworks. Welcome to my corner of the internet!
         </div>
+
+        <div
+          class="text-h6 text-center text-weight-light text-grey lt-md q-pb-lg"
+        >
+          Full Stack Developer from Virginia, USA. I create web applications
+          with modern frameworks. Welcome to my corner of the internet!
+        </div>
+
+        <q-btn
+          no-caps
+          rounded
+          size="18px"
+          class="text-weight-light gt-sm"
+          label="My Projects"
+          color="primary"
+          to="/projects"
+        />
       </div>
 
       <div class="col-12 col-md-5 col-lg-4 col-xl-3 text-center gt-sm">
